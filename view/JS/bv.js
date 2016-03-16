af041fbac3993c6d5ee26d3cfa0591378c0d593f
blomster1 <yevic.tabita@gmail.com>
2016-03-16 09:49:09 +0100

    editeur presque ok

--- view/JS/bv.js
+++ view/JS/bv.js
@@ -2,3 +2,15 @@ $(function() {
     $( "#tabs" ).tabs();
     $( "#accordion" ).accordion();
 });
+
+//EDITEUR DE text
+var doc;
+
+function initDoc(){
+    doc = document.getElementById("textBox");
+}
+
+function formatDoc(sCmd){
+    doc.focus();
+    document.execCommand(sCmd, false, null);
+}
