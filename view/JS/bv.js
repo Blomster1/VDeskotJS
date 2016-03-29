d9b9e7de7bf8610f463da7dbc7e600ab2d5b0694
blomster1 <yevic.tabita@gmail.com>
2016-03-29 17:36:00 +0200

    before push

--- view/JS/bv.js
+++ view/JS/bv.js
@@ -144,9 +144,9 @@
 							nameFile: tabTitle+"."+item.parent
 						})
 						.done(function(data){
-							var newEditDoc = '<div id="toolbar"><section><img class="intLink" title="Save" onclick="save(\''+nameFile+'\')" src="../view/libs/jsTree/icons/document-copy.png"/></section><section><img class="intLink" title="Bold" onclick="formatDoc(\'bold\');" src="../view/libs/jsTree/icons/edit-bold.png" /><img class="intLink" title="Italic" data-action="italic" src="../view/libs/jsTree/icons/edit-italic.png" /><img class="intLink" title="Underline" onclick="formatDoc(\'underline\');" src="../view/libs/jsTree/icons/edit-underline.png" /></section><section><img class="intLink" title="undo" data-action="undo" src="../view/libs/jsTree/icons/arrow-return-180-left.png" /><img class="intLink" title="redo" data-action="redo" src="../view/libs/jsTree/icons/arrow-curve.png" /></section><section><img class="intLink" title="increaseFontSize" onclick="formatDoc(\'increaseFontSize\');" src="../view/libs/jsTree/icons/edit-size-up.png" alt=""><img class="intLink" title="decreaseFontSize" onclick="formatDoc(\'decreaseFontSize\');" src="../view/libs/jsTree/icons/edit-size-down.png" alt=""></section><section id="alignment"><img class="intLink" title="left" onclick="formatDoc(\'justifyLeft\');" src="../view/libs/jsTree/icons/edit-alignment.png" alt=""><img class="intLink" title="RightAlign" onclick="formatDoc(\'justifyRight\');" src="../view/libs/jsTree/icons/edit-alignment-right.png" alt=""><img class="intLink" title="Justify" onclick="formatDoc(\'justifyFull\');" src="../view/libs/jsTree/icons/edit-alignment-justify.png" alt=""></section><section><img class="intLink" title="listUnordered" onclick="formatDoc(\'insertUnorderedList\');" src="../view/libs/jsTree/icons/edit-list.png" alt=""><img class="intLink" title="listOrdered" onclick="formatDoc(\'insertOrderedList\');" src="../view/libs/jsTree/icons/edit-list-order.png" alt=""></section></div>';
-							tabContent = newEditDoc+"<div class=\"textBox\" id=\""+nameFile+"\" contenteditable=\"true\"><p>"+data+"</p></div>";
-							tabTemplate = "<li><a href='#{href}'>#{label}</a></li>";
+							var newEditDoc = '<div id="toolbar"><section><img class="intLink" title="Save" onclick="save(this)" src="../view/libs/jsTree/icons/document-copy.png"/></section><section><img class="intLink" title="Bold" onclick="formatDoc(\'bold\');" src="../view/libs/jsTree/icons/edit-bold.png" /><img class="intLink" title="Italic" data-action="italic" src="../view/libs/jsTree/icons/edit-italic.png" /><img class="intLink" title="Underline" onclick="formatDoc(\'underline\');" src="../view/libs/jsTree/icons/edit-underline.png" /></section><section><img class="intLink" title="undo" data-action="undo" src="../view/libs/jsTree/icons/arrow-return-180-left.png" /><img class="intLink" title="redo" data-action="redo" src="../view/libs/jsTree/icons/arrow-curve.png" /></section><section><img class="intLink" title="increaseFontSize" onclick="formatDoc(\'increaseFontSize\');" src="../view/libs/jsTree/icons/edit-size-up.png" alt=""><img class="intLink" title="decreaseFontSize" onclick="formatDoc(\'decreaseFontSize\');" src="../view/libs/jsTree/icons/edit-size-down.png" alt=""></section><section id="alignment"><img class="intLink" title="left" onclick="formatDoc(\'justifyLeft\');" src="../view/libs/jsTree/icons/edit-alignment.png" alt=""><img class="intLink" title="RightAlign" onclick="formatDoc(\'justifyRight\');" src="../view/libs/jsTree/icons/edit-alignment-right.png" alt=""><img class="intLink" title="Justify" onclick="formatDoc(\'justifyFull\');" src="../view/libs/jsTree/icons/edit-alignment-justify.png" alt=""></section><section><img class="intLink" title="listUnordered" onclick="formatDoc(\'insertUnorderedList\');" src="../view/libs/jsTree/icons/edit-list.png" alt=""><img class="intLink" title="listOrdered" onclick="formatDoc(\'insertOrderedList\');" src="../view/libs/jsTree/icons/edit-list-order.png" alt=""></section></div>';
+							tabContent = newEditDoc+"<div class=\"textBox\" data-name=\""+nameFile+"\" contenteditable=\"true\">"+data+"</div>";
+							tabTemplate = "<li><a href='#{href}'>#{label} </a><span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>";
 							tabCounter = $('#tabs ul li').length+1;
 							var tabs = $("#tabs").tabs();
 					  		var label = tabTitle || "Tab " + tabCounter,
@@ -166,7 +166,11 @@
 
 		$("#tabs").tabs({
 			heightStyle: "fill"
-		});
+		}).delegate( "span.ui-icon-close", "click", function() {
+  			var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+  			$( "#" + panelId ).remove();
+  			tabs.tabs( "refresh" );
+			});
 		$("#accordion").accordion({
 			heightStyle: "fill"
 		});
@@ -177,6 +181,30 @@
 			//console.log($(this).data('action'));
 			document.execCommand($(this).data('action'), false, null);
 			//formatDoc( $(this).data('action') );
+		});
+		//CALENDAR
+		$('#calendar').eCalendar({
+			weekDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+			months: ['January', 'February', 'March', 'April', 'May', 'June',
+				'July', 'August', 'September', 'October', 'November', 'December'],
+			textArrows: {previous: '<', next: '>'},
+			eventTitle: 'Events',
+			url: '',
+			events: [{
+				title: 'Event 1',
+				description: 'Description 1',
+				datetime: new Date(2014, 7, 15, 17)
+
+			},{
+				title: 'Event 2',
+				description: 'Description 2',
+				datetime: new Date(2014, 7, 14, 16)
+
+			},{
+				title: 'Event 3',
+				description: 'jQueryScript.Net',
+				datetime: new Date(2014, 7, 10, 16)
+			}]
 		});
 	});
 
@@ -203,13 +231,13 @@
 		document.execCommand(sCmd, false, null);
 	}
 
-	function save(nameFile){
-		var contentF = $('#'+nameFile).find('p').html();
+	function save(obj){
+		var $obj = $(obj).parents('.ui-widget-content').find('.textBox');
+		var contentF = $obj.html();
 		console.log(contentF);
+		var nameFile = $obj.attr('data-name');
 		$.post('./libs/tabs/save.php',{
 			fileContent: contentF,
 			nameFile: nameFile
 		});
 	}
-
-// onclick="formatDoc('italic');"
